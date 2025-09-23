8f033c48b1f4a100183cb1398fa2b3e3858338e0
norhap <elfogondesegovia@gmail.com>
2025-09-24 01:58:59 +0200

    Remove import not used

--- weatherplugin/plugin/__init__.py
+++ weatherplugin/plugin/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS
-import os
 import gettext
 
 PluginLanguageDomain = "WeatherPlugin"
